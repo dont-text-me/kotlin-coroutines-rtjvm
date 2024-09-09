b81e7e76d1b050c8916b25342f7e463b891b1de0
dont-text-me <ivanbar3330@gmail.com>
2024-09-10 00:56:17 +0300

    more work on coroutines

---
+++ src/main/kotlin/com/ivanb/coroutines/CooperativeScheduling.kt
@@ -0,0 +1,53 @@
+package com.ivanb.coroutines
+
+import jdk.javadoc.internal.doclets.formats.html.resources.standard
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.yield
+import org.slf4j.LoggerFactory
+import kotlin.random.Random
+
+object CooperativeScheduling {
+    val logger = LoggerFactory.getLogger(CooperativeScheduling::class.java)
+
+    suspend fun greedyDeveloper() {
+        logger.info("I want all the coffee")
+        while (System.currentTimeMillis() % 10000 != 0L) {
+        }
+        logger.info("Done with coffee")
+    }
+
+    suspend fun almostGreedyDeveloper() {
+        logger.info("I want all the coffee")
+        while (System.currentTimeMillis() % 10000 != 0L) {
+            yield() // fundamental suspension point
+        }
+        logger.info("Done with coffee")
+    }
+
+    suspend fun developer(index: Int) {
+        logger.info("[dev $index]I want some coffee")
+        delay(Random.nextLong(1000))
+        logger.info("[dev $index]done with coffee")
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    suspend fun startup() {
+        logger.info("Starting up...")
+        val singleThread = Dispatchers.Default.limitedParallelism(1)
+        coroutineScope {
+            launch(context = singleThread) { almostGreedyDeveloper() }
+            launch(context = singleThread) { developer(42) }
+        }
+        logger.info("Stopping...")
+    }
+
+    // never run heavy computations in coroutines without some suspension points
+}
+
+suspend fun main() {
+    CooperativeScheduling.startup()
+}
---
+++ src/main/kotlin/com/ivanb/coroutines/StructuredConcurrency.kt
@@ -0,0 +1,77 @@
+package com.ivanb.coroutines
+
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import org.slf4j.LoggerFactory
+import java.net.URI
+import kotlin.random.Random
+
+object StructuredConcurrency {
+    val logger = LoggerFactory.getLogger(this::class.java)
+
+    suspend fun fetchHTML(url: String): String {
+        delay(1000)
+        return URI(url).toURL().readText()
+    }
+
+    suspend fun processData(data: String): String {
+        delay(500)
+        return "Processed: ${data.split("\n")[0]}"
+    }
+
+    suspend fun fetchAndProcessData(): String =
+        coroutineScope {
+            val urls = listOf("https://www.google.com", "https://www.apple.com", "https://www.samsung.com")
+            val deferredResults =
+                urls.map {
+                    async { fetchHTML(it) }
+                }
+            val results = deferredResults.awaitAll()
+
+            val deferredData =
+                results.map {
+                    async { processData(it) }
+                }
+
+            deferredData.awaitAll().joinToString("\n")
+        }
+
+    suspend fun fetchAndProcessDataNested(): String =
+        coroutineScope {
+            val urls = listOf("https://www.google.com", "https://www.apple.com", "https://www.samsung.com")
+            val htmls = coroutineScope { urls.map { async { fetchHTML(it) } }.awaitAll() }
+            val data = coroutineScope { htmls.map { async { processData(it) } }.awaitAll() }
+            data.joinToString("\n")
+        }
+
+    suspend fun fetchDataFromPage(pageUrl: String): String {
+        delay(Random.nextLong(1000))
+        return "Data from $pageUrl"
+    }
+
+    suspend fun scrape(
+        site: String,
+        pages: List<String>,
+    ): String =
+        coroutineScope {
+            pages.map { async { fetchDataFromPage("$site/$it") } }.awaitAll().joinToString("\n")
+        }
+
+    suspend fun scrapeAll(locations: List<Pair<String, List<String>>>): List<String> =
+        coroutineScope {
+            locations.map { (site, pages) -> async { scrape(site, pages) } }.awaitAll()
+        }
+}
+
+suspend fun main() {
+    println(
+        StructuredConcurrency.scrapeAll(
+            listOf(
+                "rockthejvm.com" to listOf("courses/kotlin", "courses/scala"),
+                "google.com" to listOf("search", "store"),
+            ),
+        ),
+    )
+}
